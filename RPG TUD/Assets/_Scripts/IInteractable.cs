d6bc28dc61c6947612fc21eca600b87fe700e1e3
Dobbey <dob10@live.ie>
2026-03-11 22:27:03 +0000

    Reorganized and deleted unneeded files

--- RPG TUD/Assets/_Scripts/IInteractable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-// not used yet
-
-public interface IInteractable
-{
-    void TriggerResponse(Collider other);
-}
--- RPG TUD/Assets/_Scripts/Inventory/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Item
-{
-    public string itemName;
-    public Sprite itemIcon;
-    public int stackSize = 1;
-}
